079f80f64aa5b4d5dfc98f0b35027fe9ba3b3c74
Simon Cropp <simon.cropp@gmail.com>
2012-08-14 20:55:49 +1000

    fix attributes, bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ModuleInit")]
 [assembly: AssemblyProduct("ModuleInit")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
