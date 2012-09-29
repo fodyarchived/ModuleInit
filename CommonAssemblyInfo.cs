66ad93dddcf5fadbd2c3b5b0986e6ea4eea5c275
Simon Cropp <simon.cropp@gmail.com>
2012-09-29 22:07:36 +1000

    fix nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ModuleInit")]
 [assembly: AssemblyProduct("ModuleInit")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
