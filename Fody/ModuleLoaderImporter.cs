facb8d733c4edac8e1955f9a62524955d074b245
Simon Cropp <simon.cropp@gmail.com>
2013-02-07 21:59:09 +1100

    fix minor issue with return order

--- Fody/ModuleLoaderImporter.cs
+++ Fody/ModuleLoaderImporter.cs
@@ -8,8 +8,6 @@ public class ModuleLoaderImporter
     ModuleWeaver moduleWeaver;
     InitializeMethodFinder initializeMethodFinder;
     TypeSystem typeSystem;
-    MethodDefinition cctor;
-    bool isNewCctor;
 
     public ModuleLoaderImporter(ModuleWeaver moduleWeaver, InitializeMethodFinder initializeMethodFinder, TypeSystem typeSystem)
     {
@@ -20,32 +18,21 @@ public class ModuleLoaderImporter
 
     public void Execute()
     {
-        GetCctor();
-        var il = cctor.Body.GetILProcessor();
-        il.Append(il.Create(OpCodes.Call, initializeMethodFinder.InitializeMethod));
-        if (isNewCctor)
-        {
-            il.Append(il.Create(OpCodes.Ret));
-        }
-    }
-
-    void GetCctor()
-    {
-        const MethodAttributes attributes = MethodAttributes.Static
-                                            | MethodAttributes.SpecialName
-                                            | MethodAttributes.RTSpecialName;
+        var attributes = MethodAttributes.Static
+                         | MethodAttributes.SpecialName
+                         | MethodAttributes.RTSpecialName;
         var moduleClass = moduleWeaver.ModuleDefinition.Types.FirstOrDefault(x => x.Name == "<Module>");
         if (moduleClass == null)
         {
             throw new WeavingException("Found no module class!");
         }
-         cctor = moduleClass.Methods.FirstOrDefault(x => x.Name == ".cctor");
+        var cctor = moduleClass.Methods.FirstOrDefault(x => x.Name == ".cctor");
         if (cctor == null)
         {
-            isNewCctor = true;
             cctor = new MethodDefinition(".cctor", attributes, typeSystem.Void);
             moduleClass.Methods.Add(cctor);
+            cctor.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
         }
-        
+        cctor.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Call, initializeMethodFinder.InitializeMethod));
     }
 }
