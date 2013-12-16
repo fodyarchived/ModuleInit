ff15f539915d03514b2eb80c3bcd00f9051aa9b1
Simon Cropp <simon.cropp@gmail.com>
2013-12-17 08:21:09 +1100

    make it a dev nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("ModuleInit")]
 [assembly: AssemblyProduct("ModuleInit")]
-[assembly: AssemblyVersion("1.5.4")]
-[assembly: AssemblyFileVersion("1.5.4")]
+[assembly: AssemblyVersion("1.5.5")]
+[assembly: AssemblyFileVersion("1.5.5")]
