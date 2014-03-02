2c6a4f7bed0785ee96d185613375f29059f82eaf
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:20:18 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("ModuleInit")]
 [assembly: AssemblyProduct("ModuleInit")]
-[assembly: AssemblyVersion("1.5.5")]
-[assembly: AssemblyFileVersion("1.5.5")]
+[assembly: AssemblyVersion("1.5.6")]
+[assembly: AssemblyFileVersion("1.5.6")]
