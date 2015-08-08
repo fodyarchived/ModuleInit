0bc70c9799b028ca7d87cb023f712b3b4f370012
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 13:29:25 +1000

    better universal support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("ModuleInit")]
 [assembly: AssemblyProduct("ModuleInit")]
-[assembly: AssemblyVersion("1.5.6")]
-[assembly: AssemblyFileVersion("1.5.6")]
+[assembly: AssemblyVersion("1.5.7")]
+[assembly: AssemblyFileVersion("1.5.7")]
